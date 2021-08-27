2164e16b6ffb3b78398835a586ecdfd9ab75bc2e
dhvcc <1337kwiz@gmail.com>
2021-08-27 20:18:00 +0300

    Basic sentry config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+const { withSentryConfig } = require("@sentry/nextjs");
 const withPWA = require('next-pwa');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
@@ -6,7 +7,7 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-module.exports = withBundleAnalyzer(withPWA({
+const moduleExports = withBundleAnalyzer(withPWA({
   pwa: {
     disable: process.env.NODE_ENV === 'development',
     dest: 'public',
@@ -16,5 +17,14 @@ module.exports = withBundleAnalyzer(withPWA({
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
     BACKEND_PORT: process.env.BACKEND_PORT,
+    SENTRY_URL: process.env.SENTRY_URL,
+    SENTRY_DSN: process.env.SENTRY_DSN,
+    SENTRY_ORG: process.env.SENTRY_ORG,
+    SENTRY_PROJECT: process.env.SENTRY_PROJECT,
+    SENTRY_AUTH_TOKEN: process.env.SENTRY_AUTH_TOKEN,
   },
 }));
+
+const SentryWebpackPluginOptions = {};
+
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
