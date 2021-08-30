b877cc1c77a6e800fbc9c16258ddea16108380f8
dhvcc <1337kwiz@gmail.com>
2021-08-30 15:46:28 +0300

    Redux persist
    Removed old way of syncing localStorage

--- src/redux/manga/utils.ts
+++ src/redux/manga/utils.ts
@@ -1,7 +1,4 @@
-import { useEffect } from 'react';
-import isEmpty from 'lodash.isempty';
-import { loadViewedManga } from './actions';
-import { Manga, MangaList } from '../../utils/apiTypes';
+import { Manga } from '../../utils/apiTypes';
 
 export const detailsNeedUpdate = (manga: Manga) => {
   if (!manga.updatedDetail) return false;
@@ -16,28 +13,3 @@ export const chaptersNeedUpdate = (manga: Manga) => {
   hourAgo.setHours(hourAgo.getHours() - 1);
   return new Date(manga.updatedChapters) < hourAgo;
 };
-
-// Load viewed from localstorage if needed
-export const useSyncViewed = (dispatch: any, clientState: MangaList) =>
-  useEffect(() => {
-    const data = window.localStorage.getItem('sora-viewed') || '[]';
-    const cachedState = JSON.parse(String(data));
-
-    const jsonClientState = JSON.stringify(clientState);
-    const needsUpdate = jsonClientState !== JSON.stringify(cachedState);
-
-    // If they're not equal
-    if (needsUpdate) {
-      // And client's state is empty (first render)
-      if (isEmpty(clientState)) {
-        // Then load cached state
-        const cachedViewed: Array<Manga> = cachedState;
-        dispatch(loadViewedManga(cachedViewed));
-      }
-      // But if client state has something different, than cache
-      else {
-        // Then it means we need to sync localStorage
-        window.localStorage.setItem('sora-viewed', jsonClientState);
-      }
-    }
-  }, [clientState, dispatch]);
