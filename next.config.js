634fac205c20d1ae1148fde6fe97d102ef4fe4bc
Alexey Artishevskiy <1337kwiz@gmail.com>
2022-01-01 00:43:42 +0300

    Formatted next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
 dotenv.config();
 
@@ -18,27 +18,26 @@ const bundleAnalyzerConfig = {
   enabled: true,
 };
 const pwaConfig = {
-  disable: process.env.NODE_ENV === 'development',
-  dest: 'public',
+  disable: process.env.NODE_ENV === "development",
+  dest: "public",
   register: true,
   skipWaiting: true,
-}
+};
 const sentryConfig = {};
 
-if (process.env.NODE_ENV !== 'development') {
-  const withPWA = require('next-pwa');
+if (process.env.NODE_ENV !== "development") {
+  const withPWA = require("next-pwa");
   module.exports = withPWA({
     ...module.exports,
     pwa: pwaConfig,
   });
 
-  if (process.env.SENTRY_DSN) 
-  {
+  if (process.env.SENTRY_DSN) {
     const { withSentryConfig } = require("@sentry/nextjs");
-    module.exports = withSentryConfig(module.exports, sentryConfig); 
+    module.exports = withSentryConfig(module.exports, sentryConfig);
   }
-  if (process.env.ANALYZE === 'true') {
-    const withBundleAnalyzer = require('@next/bundle-analyzer')()
+  if (process.env.ANALYZE === "true") {
+    const withBundleAnalyzer = require("@next/bundle-analyzer")();
     module.exports = withBundleAnalyzer(module.exports, bundleAnalyzerConfig);
   }
 }
@@ -46,7 +45,7 @@ if (process.env.NODE_ENV !== 'development') {
 /**
  * Remove undefined values so Next.js doesn't complain during serialization. Verified as of v11.1.2.
  */
- const removeUndefined = (obj) => {
+const removeUndefined = (obj) => {
   let newObj = {};
   Object.keys(obj).forEach((key) => {
     if (obj[key] === Object(obj[key])) newObj[key] = removeUndefined(obj[key]);
@@ -54,6 +53,7 @@ if (process.env.NODE_ENV !== 'development') {
   });
   return newObj;
 };
-const next = require('next/dist/lib/is-serializable-props');
+const next = require("next/dist/lib/is-serializable-props");
 const isSerializableProps = next.isSerializableProps;
-next.isSerializableProps = (page, method, input) => isSerializableProps(page, method, removeUndefined(input));
+next.isSerializableProps = (page, method, input) =>
+  isSerializableProps(page, method, removeUndefined(input));
